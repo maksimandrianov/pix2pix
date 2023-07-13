12c4f3485ba0a01603966e43516334ecf8c5828d
Maksim Andrianov <maksimandrianov1@gmail.com>
2023-07-14 02:50:39 +0300

    Fix display

--- net/utils.py
+++ net/utils.py
@@ -25,25 +25,25 @@ def set_grad(nets, grad):
 def display(input_img, output_img, target_img=None, path=None):
     input_img = input_img.detach().cpu().numpy()
     input_img = input_img.squeeze().transpose((1, 2, 0))
-    input_img = (input_img * 255).astype("uint8")
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 3, 1)
     ax1.set_title("Input")
+    input_img = input_img / 2 + 0.5
     ax1.imshow(input_img)
 
     output_img = output_img.detach().cpu().numpy()
     output_img = output_img.squeeze().transpose((1, 2, 0))
-    output_img = (output_img * 255).astype("uint8")
     ax2 = fig.add_subplot(1, 3, 2)
     ax2.set_title("Output")
+    output_img = output_img / 2 + 0.5
     ax2.imshow(output_img)
 
     if target_img is not None:
         target_img = target_img.detach().cpu().numpy()
         target_img = target_img.squeeze().transpose((1, 2, 0))
-        target_img = (target_img * 255).astype("uint8")
         ax3 = fig.add_subplot(1, 3, 3)
         ax3.set_title("Target")
+        target_img = target_img / 2 + 0.5
         ax3.imshow(target_img)
 
     plt.show()
